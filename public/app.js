e06e7ef7dc5cce80c6b73078b8d35796568b565b
Christopher Jennison <cjennison92@gmail.com>
2017-01-11 09:31:47 -0500

    angular 4.12 and a giant celebration thing

--- public/app.js
+++ public/app.js
@@ -1,33 +1,68 @@
 var pbxxTv = angular.module('pbxxTv', [
+	'ui.router'
 		//ng-deps
-	])
+])
+.config(['$stateProvider', function($stateProvider) {
+    $stateProvider.state("home", {
+      url: '/',
+      controller: function ($scope, $http, $timeout) {
+				$scope.company = "Promoboxx";
+				$scope.data = {
+					celebration: {
+						on: false,
+						reason: "CELEBRATE, YOU FOOLS",
+						subreason: "Run Frodo, bring the ring to Mt. Doom"
+					},
+					retailers: {
+						count: 0,
+						change: 0
+					}
+				}
 
-function mainController ($scope, $http) {
-	$scope.company = "Promoboxx";
-	$scope.data = {
-		retailerCount: 0
-	}
+				function toggleChange (key, change) {
+					$scope.data[key].change = change;
+					$timeout(function () {
+						$scope.data[key].change = null;
+					}, 1500)
+				}
 
-	function pingServer () {
-		console.log('refreshing')
+				function toggleCelebration () {
+					$scope.data.celebration.on = true;
 
-		$http.get('/retailers')
-		.success(function (data) {
-			$scope.data.retailerCount = data.num_retailers;
-		})
-		.error(function (err) {
-			console.warn(err)
-		})
+					$timeout(function () {
+						$scope.data.celebration.on = false;		
+					}, 3000)
+				}
 
-		setTimeout(function () {
-			pingServer();
-		}, 100000)
-	}
+				function pingServer () {
+					console.log('refreshing')
+
+					toggleCelebration();
+
+					$http.get('/retailers')
+					.success(function (data) {
+						$scope.data.retailers.count = data.num_retailers;
+						toggleChange('retailers', 120)
+
+					})
+					.error(function (err) {
+						console.warn(err)
+					})
+
+					$timeout(function () {
+						pingServer();
+					}, 100000)
+				}
 
 
-	function init () {
-		pingServer();
-	}
+				function init () {
+					$timeout(function () {
+						pingServer();
+					})
+				}
 
-	init();
-}
+				init();
+			}
+    });
+  }])
+
