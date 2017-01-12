5174302cfd0fbaf6708ae693a605d2e480e08c63
Christopher Jennison <cjennison92@gmail.com>
2017-01-12 16:42:17 -0500

    a lot

--- public/app.js
+++ public/app.js
@@ -15,8 +15,70 @@ var pbxxTv = angular.module('pbxxTv', [
 					},
 					retailers: {
 						count: 0,
+						eligibleCount: 0,
+						change: 0
+					},
+					brands: {
+						active: 0,
+						unlocked: 0,
+						change: 0
+					},
+					brand_campaigns: {
+						present: 0,
+						past: 0,
+						change: 0,
+						percent_change: 0
+					},
+					cd_campaigns: {
+						present: 0,
+						past: 0,
+						change: 0,
+						percent_change: 0
+					},
+					shares: {
+						facebook: {
+							present: 0,
+							past: 0,
+							percent_change: 0
+						},
+						twitter: {
+							present: 0,
+							past: 0,
+							percent_change: 0
+
+						},
+						change: 0
+					},
+					brand_requests: {
+						count: 0,
+						unique: 0,
+						change: 0
+					},
+					engaged_billing_locations: {
+						count: 0,
+						change: 0
+					},
+					ad_runs: {
+						total: 0,
+						rf: 0,
+						bf: 0,
+						change: 0
+					},
+					engaged_locations: {
+						present: {
+							num_retailers_engaged: 0
+						},
+						past: {
+							num_retailers_engaged: 0
+						},
+						billing_count: 0,
+						percent_change: 0,
 						change: 0
 					}
+				}
+
+				$scope.metQuarterGoal = function (goal, base, earned) {
+					return Math.abs(goal - base) <= earned
 				}
 
 				function toggleChange (key, change) {
@@ -26,33 +88,155 @@ var pbxxTv = angular.module('pbxxTv', [
 					}, 1500)
 				}
 
+				function getPercentDiff (historic, current) {
+					return (current - historic)/historic * 100;
+				}
+
 				function toggleCelebration () {
 					//TODO: Re-enable eventually
-					//$scope.data.celebration.on = true;
+					$scope.data.celebration.on = true;
 
 					$timeout(function () {
 						$scope.data.celebration.on = false;		
-					}, 3000)
+					}, 15000)
 				}
 
 				function pingServer () {
 					console.log('refreshing')
+					$scope.refreshed = true;
 
-					toggleCelebration();
+					//toggleCelebration();
 
 					$http.get('/retailers')
 					.success(function (data) {
-						$scope.data.retailers.count = data.num_retailers;
-						toggleChange('retailers', 120)
+						var engaged = _.find(data, function (row) { return row.state === 'engaged'}).num_retailers
+						var eligible = _.find(data, function (row) { return row.state === 'eligible'}).num_retailers
+
+						toggleChange('retailers', (engaged - $scope.data.retailers.count))
+						$scope.data.retailers.count = engaged;
+						$scope.data.retailers.eligibleCount = eligible
+					})
+
+					$http.get('/brand_campaigns')
+					.success(function (data) {
+
+						var concat_date = moment().format('YYYY-MM')
+						var prev_date = moment().subtract(1, 'month').format('YYYY-MM')
+
+						var present = _.find(data, function (row) { return row['date'] == concat_date})
+						var past = _.find(data, function (row) { return row['date'] == prev_date})
+						toggleChange('brand_campaigns', (present.campaigns_launched - $scope.data.brand_campaigns.present))
+
+						$scope.data.brand_campaigns.present = present.campaigns_launched;
+						$scope.data.brand_campaigns.past = past.campaigns_launched;
+
+						$scope.data.brand_campaigns.percent_change = getPercentDiff(past.campaigns_launched, present.campaigns_launched)
+					})
+
+					$http.get('/cd_campaigns')
+					.success(function (data) {
+
+						var concat_date = moment().format('YYYY-MM')
+						var prev_date = moment().subtract(1, 'month').format('YYYY-MM')
+
+						var present = _.find(data, function (row) { return row['date'] == concat_date})
+						var past = _.find(data, function (row) { return row['date'] == prev_date})
+						toggleChange('cd_campaigns', (present.campaigns_launched - $scope.data.cd_campaigns.present))
+
+						$scope.data.cd_campaigns.present = present.campaigns_launched;
+						$scope.data.cd_campaigns.past = past.campaigns_launched;
+
+						$scope.data.cd_campaigns.percent_change = getPercentDiff(past.campaigns_launched, present.campaigns_launched)
+					})
+
+					$http.get('/brands')
+					.success(function (data) {
+						var active = _.find(data, function (row) { return row.status === 'active'});
+						var unlocked = _.find(data, function (row) { return row.status === 'unlocked'});
+						toggleChange('brands', (active.brand_count - $scope.data.brands.active))
+						$scope.data.brands.active = active.brand_count; 
+						$scope.data.brands.unlocked = unlocked.brand_count;
+					})
+
+					$http.get('/monthly_engaged_locations')
+					.success(function (data) {
+						var present = data[data.length-1];
+						var past = data[data.length-2]
+						toggleChange('engaged_locations', (present.num_retailers_engaged - $scope.data.engaged_locations.present.num_retailers_engaged))
+						$scope.data.engaged_locations.present = present;
+						$scope.data.engaged_locations.past = past;
+					})
+
+					$http.get('/brand_requests')
+					.success(function (data) {
+						toggleChange('brand_requests', (data.count - $scope.data.brand_requests.count))
+						$scope.data.brand_requests.count = data.count;
+						$scope.data.brand_requests.unique = data.count_brand_ids;
+					})
+
+					$http.get('/engaged_locations')
+					.success(function (data) {
+						toggleChange('engaged_billing_locations', (data.count - $scope.data.engaged_billing_locations.count))
+						$scope.data.engaged_billing_locations.count = data.count;
+					})
+
+					$http.get('/arr')
+					.success(function (data) {
+						console.log(data)
+						$scope.data.arr = data;
+					})
+
+					$http.get('/ad_runs')
+					.success(function (data) {
+						console.log(data)
+						var rf = _.find(data, function (r) { return r['is_rf'] === 1}).count;
+						var bf = _.find(data, function (r) { return r['is_rf'] === 0}).count;
+						var sum = rf + bf;
+						toggleChange('ad_runs', (sum - $scope.data.ad_runs.total))
+						$scope.data.ad_runs.total = sum;
+						$scope.data.ad_runs.rf = rf;
+						$scope.data.ad_runs.bf = bf;
 
 					})
-					.error(function (err) {
-						console.warn(err)
+
+					$http.get('/celebration_reasons')
+					.success(function (data) {
+						if (data) {
+							$scope.data.celebration.reason = data.title;
+							$scope.data.celebration.subreason = data.subtitle
+							toggleCelebration();
+						}
+					})
+
+					$http.get('/shares')
+					.success(function (data) {
+						var concat_date = moment().format('YYYY-MM')
+						var prev_date = moment().subtract(1, 'month').format('YYYY-MM')
+
+						var fbpresent = _.find(data, function (row) { return row['date'] == concat_date && row['type'] === 'facebook'}).num_shares_made
+						var fbpast = _.find(data, function (row) { return row['date'] == prev_date && row['type'] === 'facebook'}).num_shares_made
+
+						var twpresent = _.find(data, function (row) { return row['date'] == concat_date && row['type'] === 'twitter'}).num_shares_made
+						var twpast = _.find(data, function (row) { return row['date'] == prev_date && row['type'] === 'twitter'}).num_shares_made
+
+						var sum = fbpresent + twpresent;
+						var storedSum = $scope.data.shares.facebook.present + $scope.data.shares.twitter.present;
+
+						toggleChange('shares', (sum - storedSum))
+						$scope.data.shares.facebook.present = fbpresent;
+						$scope.data.shares.facebook.past = fbpast;
+						$scope.data.shares.twitter.present = twpresent;
+						$scope.data.shares.twitter.past = twpast;
+
+						$scope.data.shares.facebook.percent_change = getPercentDiff(fbpast, fbpresent)
+						$scope.data.shares.twitter.percent_change = getPercentDiff(twpast, fbpresent)
+
 					})
 
 					$timeout(function () {
+						$scope.refreshed = false;
 						pingServer();
-					}, 100000)
+					}, 60000)
 				}
 
 
@@ -60,10 +244,45 @@ var pbxxTv = angular.module('pbxxTv', [
 					$timeout(function () {
 						pingServer();
 					})
+
+					$scope.date = moment().format();
+
+					$scope.thisMonth = moment().month()+1;
 				}
 
 				init();
 			}
-    });
+    })
+		.state('updater', {
+			url: '/update',
+			templateUrl: 'update-form.tmpl.html',
+			controller: function ($scope, $http) {
+
+				$scope.data = {
+					brand_slug: null,
+					arr: null,
+					celebrate: false,
+					celebration_title: null,
+					celebration_subtitle: null
+				}
+
+				$scope.submit = function () {
+
+					if (!$scope.data.brand_slug || !$scope.data.arr) {
+						console.warn("Why did you enter in nothing? ...");
+						return;
+					}
+
+					$http.post('/brand', $scope.data)
+						.success(function () {
+							console.log("gewd")
+						})
+						.error(function (err) {
+							console.warn("badd", err)
+						})
+				}
+
+			}
+		})
   }])
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');    // pull information from HTML POST (
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var yaml = require('js-yaml');
 var fs = require('fs');
+var moment = require('moment');
 var port = 9844;
 
 var Sequelize = require('sequelize');
@@ -12,6 +13,10 @@ var Sequelize = require('sequelize');
 var config_dir = __dirname + "/config";
 var db_config = yaml.load(fs.readFileSync(config_dir + "/db_config.yml", 'utf8'));
 var sequelize = new Sequelize(db_config.db);
+
+var celebrations = require('./lib/CelebrationService');
+var arr = require('./lib/ARRService');
+
 
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
@@ -22,25 +27,152 @@ app.use(methodOverride());
 
 // routes
 app.get('/retailers', function (req, res) {
-  sequelize.query("SELECT COUNT(*) as num_retailers FROM retailers WHERE state = 'engaged'", { type: sequelize.QueryTypes.SELECT})
-  .then(function(result) {
-    // We don't need spread here, since only the results will be returned for select queries
-    res.json(result[0])
+  sequelize.query("SELECT state, COUNT(*) as num_retailers " +
+									"FROM pbxx.retailers " +
+									"WHERE deleted_at IS NULL " +
+									"GROUP BY state" , { type: sequelize.QueryTypes.SELECT})
+  .then(function(result_count) {
+    	res.json(result_count)
+  	})
   })
-})
 
 app.get('/brands', function (req, res) {
-  res.send("A million")
+  sequelize.query("SELECT brands.status, COUNT(*) as brand_count " +
+										"FROM brands " +
+										"WHERE brands.deleted_at IS NULL " +
+										"AND slug NOT LIKE '%promoboxx%' " +
+										"GROUP BY brands.status", { type: sequelize.QueryTypes.SELECT})
+  	.then(function (results) {
+  		res.json(results)
+  	})
+})
+
+app.get('/brand_campaigns', function (req, res) {
+	sequelize.query("SELECT DATE_FORMAT(start_date, '%Y-%m') AS date, COUNT(*) as campaigns_launched " +
+									"FROM campaigns " +
+									"WHERE deleted_at IS NULL " +
+									"  AND campaign_type <> 'retailer_discovered' " +
+									"GROUP BY DATE_FORMAT(start_date, '%M-%Y') " +
+									"ORDER BY DATE_FORMAT(start_date, '%Y-%m') DESC", { type: sequelize.QueryTypes.SELECT})
+	.then(function (results) {
+		res.json(results)
+	})
+})
+
+app.get('/shares', function (req, res) {
+	sequelize.query("SELECT * " +
+									"FROM ( " +
+									"SELECT DATE_FORMAT(executed_at, '%Y-%m') as date, 'facebook' as type, COUNT(*) as num_shares_made " +
+									"FROM facebook_shares " +
+									"GROUP BY DATE_FORMAT(executed_at, '%Y-%m') " +
+									"UNION " +
+									"SELECT DATE_FORMAT(executed_at, '%Y-%m') as date, 'twitter' as type, COUNT(*) as num_shares_made " +
+									"FROM twitter_shares " +
+									"GROUP BY DATE_FORMAT(executed_at, '%Y-%m') " +
+									") AS E", { type: sequelize.QueryTypes.SELECT})
+	.then(function (results) {
+		res.json(results)
+	}, function (err) {
+		console.log(err)
+	})
+})
+
+app.get('/cd_campaigns', function (req, res) {
+	sequelize.query("SELECT DATE_FORMAT(start_date, '%Y-%m') AS date, COUNT(*) as campaigns_launched " +
+									"FROM campaigns " +
+									"WHERE deleted_at IS NULL " +
+									"  AND campaign_type = 'retailer_discovered' " +
+									"GROUP BY DATE_FORMAT(start_date, '%M-%Y') " +
+									"ORDER BY DATE_FORMAT(start_date, '%Y-%m') DESC", { type: sequelize.QueryTypes.SELECT})
+	.then(function (results) {
+		res.json(results)
+	})
+})
+
+app.get('/ad_runs', function (req, res) {
+
+	sequelize.query("SELECT intended_self_fund_amount_cents > 0 as is_rf, COUNT(*) as count " +
+									"FROM ad_runs " +
+									"WHERE created_at > '2017-01-01' AND status = 'complete' AND deleted_at IS NULL " +
+									"GROUP BY intended_self_fund_amount_cents > 0", { type: sequelize.QueryTypes.SELECT })
+	.then(function (results) {
+		res.json(results)
+	})
+
+})
+
+app.get('/brand_requests', function (req, res) {
+
+	sequelize.query("SELECT COUNT(*) as count " +
+									"FROM brand_requests " +
+									"WHERE deleted_at IS NULL", { type: sequelize.QueryTypes.SELECT })
+	.then(function (results) {
+		sequelize.query("SELECT COUNT(DISTINCT brand_id) as count " +
+									"FROM brand_requests " +
+									"WHERE deleted_at IS NULL", { type: sequelize.QueryTypes.SELECT })
+		.then(function (result_unique) {
+			res.json({
+				count: results[0].count,
+				count_brand_ids: result_unique[0].count
+			})
+		})
+	})
+
+})
+
+app.get('/monthly_engaged_locations', function (req, res) {
+	sequelize.query("SELECT E_first.event_year, E_first.event_month, DATE_ADD(MAKEDATE(E_first.event_year, 1), INTERVAL (E_first.event_month)-1 MONTH) AS date, COUNT(*) AS num_retailers_engaged FROM (SELECT E.retailer_id, E.event_year, E.event_month FROM (SELECT events.retailer_id AS retailer_id, YEAR(events.created_at) AS event_year, MONTH(events.created_at) AS event_month, events.created_at FROM events WHERE ((event_type = 'campaign' AND category = 'share' AND events.subcategory IN ('facebook', 'twitter', 'email', 'instagram', 'mobile_ad')) OR (event_type = 'campaign' AND category = 'install' AND events.subcategory = 'widget') OR (event_type = 'campaign' AND category = 'download' AND events.subcategory = 'asset')) AND events.created_at >= '2016-10-01' AND deleted_at IS NULL) AS E GROUP BY E.retailer_id, event_year, event_month ) AS E_first JOIN retailers ON retailers.id = E_first.retailer_id AND retailers.deleted_at IS NULL LEFT OUTER JOIN locations ON locations.locationable_type = 'Retailer' AND locations.deleted_at IS NULL AND locations.locationable_id = retailers.id GROUP BY E_first.event_year, E_first.event_month", { type: sequelize.QueryTypes.SELECT})
+  .then(function(result) {
+  	res.json(result)
+  });
 })
 
 app.get('/engaged_locations', function (req, res) {
-  res.send("A Killamenjaro-ian")
+	sequelize.query("SELECT COUNT(*) as count FROM locations RIGHT OUTER JOIN retailers ON locations.locationable_type = 'Retailer' AND locations.locationable_id = retailers.id WHERE locations.deleted_at IS NULL AND retailers.id IN ( SELECT DISTINCT retailers.id FROM retailers JOIN permissions ON permissions.permissionable_type = 'Retailer' AND permissions.permissionable_id = retailers.id JOIN users ON users.id = permissions.user_id WHERE retailers.state = 'engaged' AND retailers.deleted_at IS NULL )", {type: sequelize.QueryTypes.SELECT})
+	.then(function (results) {
+		res.json(results[0])
+	})
+})
+
+app.get('/arr', function (req, res) {
+	arr.getARRSummary(function (summary) {
+		res.json(summary)
+	});
+})
+
+
+app.post('/brand', function (req, res) {
+	//Check if the brand exists..
+	var brandSlug = req.body.brand_slug;
+
+	sequelize.query("SELECT * FROM brands WHERE slug = '" + brandSlug + "'", { type: sequelize.QueryTypes.SELECT })
+		.then(function (brands) {
+			if (!brands.length) {
+				res.status(400).send("That brand doesn't exist. Stop it.")
+				return;
+			}
+
+			if (req.body.celebrate) {
+				celebrations.addCelebration(req.body.celebration_title, req.body.celebration_subtitle)
+			}
+
+			arr.updateBrand(req.body, function () {
+				res.send("Good")
+			})
+		})
+})
+
+app.get('/celebration_reasons', function (req, res) {
+	res.json(celebrations.getNextCelebration())
 })
 
 app.get('*', function (req, res) {
   res.sendFile('./public/index.html')
 })
 
+
+
+var timeNow = moment().subtract(moment().date()-1, 'days').format('YYYY-MM-DD');
 
 app.listen(port);
 console.log("App listening on port ", port);
